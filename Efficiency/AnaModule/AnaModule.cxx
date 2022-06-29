d71c083534f810969f9eea817e1ab3baccdc87ea
dinupa1 <dinupa3@gmail.com>
2022-06-29 09:31:40 -0500

    minor bug

--- Efficiency/AnaModule/AnaModule.cxx
+++ Efficiency/AnaModule/AnaModule.cxx
@@ -43,8 +43,8 @@ int AnaModule::process_event(PHCompositeNode* topNode)
     
     // get only acctepted NIM4 || MATRIX5 events
     // beam like and reverse beam like
-    //if(!(event->get_trigger(SQEvent::NIM4)|| event->get_trigger(SQEvent::MATRIX5))) continue;
-    if(!event->get_trigger(SQEvent::NIM4)) continue;
+    if(!(event->get_trigger(SQEvent::NIM4)|| event->get_trigger(SQEvent::MATRIX5))) continue;
+    //if(!event->get_trigger(SQEvent::NIM4)) continue;
     if(!acc_h4(tracklet)) continue;
 
     //very loose cuts here
@@ -237,15 +237,15 @@ void AnaModule::effi_h4(Tracklet* tracklet)
   for(int i = 0; i < nhodo; i++)
   {
     int det_id = hodo4.at(i);
-    //int exp_id = fit_prop(det_id, tracklet);
+    int exp_id = fit_prop(det_id, tracklet);
     
-    double z_exp = p_geomSvc->getPlanePosition(det_id);
-    double x_exp = tracklet->getExpPositionX(z_exp);
-    double y_exp = tracklet->getExpPositionY(z_exp);
+    //double z_exp = p_geomSvc->getPlanePosition(det_id);
+    //double x_exp = tracklet->getExpPositionX(z_exp);
+    //double y_exp = tracklet->getExpPositionY(z_exp);
     
-    if(!p_geomSvc->isInPlane(det_id, x_exp, y_exp)) continue;
+    //if(!p_geomSvc->isInPlane(det_id, x_exp, y_exp)) continue;
     
-    int exp_id = p_geomSvc->getExpElementID(det_id, tracklet->getExpPositionW(det_id));
+    //int exp_id = p_geomSvc->getExpElementID(det_id, tracklet->getExpPositionW(det_id));
     
     if(exp_id < 1 || exp_id > p_geomSvc->getPlaneNElements(det_id)) continue;
     
