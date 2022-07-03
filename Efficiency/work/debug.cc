e0a2fe09777beef0e39179036ee93d2a593cca01
dinupa1 <dinupa3@gmail.com>
2022-07-03 16:52:57 -0500

    minor bug

--- Efficiency/work/debug.cc
+++ Efficiency/work/debug.cc
@@ -1,20 +1,18 @@
 #include "model.cc"
+#include <vector>
+#include <iostream>
 
-//using namespace std;
+using namespace std;
 
 void debug()
-{
-	model* m = new model();
-	
+{	
 	// set hodo ids
-	//41, 42, 43, 44, 45, 46
-	m->set_det(41);
-	m->set_det(42);
-	m->set_det(43);
-	m->set_det(44);
-  m->set_det(45);
-  m->set_det(46);
+	vector<int> hodo = {41, 42, 43, 44, 45, 46};
+	int nhodo = hodo.size();
 
+	// do ana
+	model* m = new model();	
+	for(int i = 0; i < nhodo; i++){m->set_det(hodo.at(i))};
 	m->ana();
 	m->done();
 }
