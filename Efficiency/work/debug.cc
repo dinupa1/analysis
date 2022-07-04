47324a6665a2d87be2eff16b68a48ee4f87c031a
dinupa1 <dinupa3@gmail.com>
2022-07-04 15:28:44 -0500

    minor bug

--- Efficiency/work/debug.cc
+++ Efficiency/work/debug.cc
@@ -16,8 +16,8 @@ void debug()
 	model* m = new model();	
 	for(int i = 0; i < nhodo; i++)
 	{
-		//m->set_det(hodoid.at(i));
-		cout << hodoid.at(i) << endl;
+		int id = hodoid.at(i);
+		m->set_det(id);
 	}
 	//m->ana();
 	///m->done();
