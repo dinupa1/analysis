96c800d6e2a3a20fb3458504b55683a27651c394
dinupa1 <dinupa3@gmail.com>
2022-07-04 15:22:07 -0500

    minor bug

--- Efficiency/work/debug.cc
+++ Efficiency/work/debug.cc
@@ -10,14 +10,15 @@ void debug()
 	vector<int> hodo = {41, 42, 43, 44, 45, 46};
 	int nhodo = hodo.size();
 
-	cout << nhodo << endl;
+	//cout << nhodo << endl;
 
 	// do ana
-	/*model* m = new model();	
+	model* m = new model();	
 	for(int i = 0; i < nhodo; i++)
 	{
 		m->set_det(hodo.at(i));
+		cout << hodo.at(i) << endl;
 	}
 	m->ana();
-	m->done();*/
+	m->done();
 }
